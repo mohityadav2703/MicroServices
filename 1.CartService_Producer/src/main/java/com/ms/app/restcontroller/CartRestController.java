8958480a533de00cbd9e5a4debee1cfc34d60100
Mohit Kumar <143256804+yadav-mohit-17@users.noreply.github.com>
2025-01-26 18:59:27 +0530

    update

--- 1.CartService_Producer/src/main/java/com/ms/app/restcontroller/CartRestController.java
+++ 1.CartService_Producer/src/main/java/com/ms/app/restcontroller/CartRestController.java
@@ -1,7 +1,6 @@
 package com.ms.app.restcontroller;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,7 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/cart")
-@RefreshScope
 public class CartRestController {
 
 	@Value("${my.app.title}")
@@ -20,6 +18,6 @@ public class CartRestController {
 	
 	@GetMapping("/message")
 	public ResponseEntity<String> cartMessage(){
-		return ResponseEntity.ok("FROM CART SERVICE :"+port +title);
+		return ResponseEntity.ok("FROM CART SERVICE :"+ port +title);
 	}
 }
